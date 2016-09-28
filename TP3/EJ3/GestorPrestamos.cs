c091bd002905f23925df81fb94d32d1d6bb787b3
Black <Black@BLACK-NET>
2016-09-28 18:11:36 -0300

    Creado Gestor de prestamos. Falta definir los EvaluadoresCompuestos

---
+++ TP3/EJ3/GestorPrestamos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ3
+{
+    class GestorPrestamos
+    {
+
+        Dictionary<TipoCliente, IEvaluador> iEvaluadoresPorCliente;
+
+        public bool EsValida(SolicitudPrestamo pSolicitud)
+        {
+            return iEvaluadoresPorCliente[pSolicitud.Cliente.TipoCliente].EsValida(pSolicitud);
+        }
+    }
+}
