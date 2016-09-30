d29af3340265094bf63136aa2154003f82c57eca
Manuel Chichi <manuchichi@hotmail.com>
2016-09-30 20:14:09 -0300

    Definido

--- TP3/EJ4/EncriptadorAES.cs
+++ TP3/EJ4/EncriptadorAES.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace EJ4
 {
-    class EncriptadorAES
+    class EncriptadorAES : Encriptador
     {
+        public EncriptadorAES()
+        {
+
+        }
+
+        public string Encriptar(string pCadena)
+        {
+
+        }
+
+        public string Desencriptar(string pCadena)
+        {
+
+        }
+
     }
 }
