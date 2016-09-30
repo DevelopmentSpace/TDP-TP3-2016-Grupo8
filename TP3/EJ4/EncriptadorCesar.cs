b1a8020a538841ec74e8a60a3203245a431b417d
Black <Black@BLACK-NET>
2016-09-30 20:09:28 -0300

    Encriptador listo y EncriptadorCesar en proceso

--- TP3/EJ4/EncriptadorCesar.cs
+++ TP3/EJ4/EncriptadorCesar.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace EJ4
 {
-    class EncriptadorCesar
+    class EncriptadorCesar : Encriptador
     {
+
+        private static char[] letras ="abcdefghijklmnñopqrstuvwxyz".ToCharArray();
+        int n = letras.Length;
+
+        private int iDesplazamiento;
+
+        public EncriptadorCesar(int pDesplazamiento):base("Cesar")
+        {
+            this.iDesplazamiento = pDesplazamiento;
+        }
+
+        public override string Encriptar(string pCadena)
+        {
+            foreach(char l in pCadena)
+            {
+
+            }
+        }
+
+        public override string Desencriptar(string pCadena)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
