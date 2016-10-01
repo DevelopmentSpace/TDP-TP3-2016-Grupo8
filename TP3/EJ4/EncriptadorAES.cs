851f9207a0a6e1939b0801ce1c4a8a858253c49f
Manuel Chichi <manuchichi@hotmail.com>
2016-09-30 23:18:20 -0300

    Rijndael es basicamente lo mismo que AES.

--- TP3/EJ4/EncriptadorAES.cs
+++ TP3/EJ4/EncriptadorAES.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,12 +16,74 @@ namespace EJ4
 
         public override string Encriptar(string pCadena)
         {
-            return CifradorString.Encrypt(pCadena, "HomoSapiens"); 
+            return EncryptStringToString(pCadena); 
         }
 
         public override string Desencriptar(string pCadena)
         {
-            return CifradorString.Decrypt(pCadena, "HomoSapiens");
+            return DecryptStringFromString(pCadena);
+        }
+
+        static string EncryptStringToString(string plainText)
+        {
+            byte[] encrypted;
+            // Create an RijndaelManaged object
+            // with the specified key and IV.
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor();
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(encrypted);
+
+        }
+        static string DecryptStringFromString(string cipherText)
+        {
+            byte[] aBytes = Encoding.ASCII.GetBytes(cipherText);
+            string plaintext = null;
+
+            // Create an RijndaelManaged object
+            // with the specified key and IV.
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor();
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(aBytes))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
         }
 
     }
