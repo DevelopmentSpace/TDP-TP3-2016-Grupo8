e815fe5b24719eaebfc93ff08b59931eaf22600f
Agu <agustincaire@gmail.com>
2016-10-01 12:24:45 -0300

    Mejore la interfaz

--- TP3/EJ3/Program.cs
+++ TP3/EJ3/Program.cs
@@ -18,25 +18,37 @@ namespace EJ3
             int monto;
             int cantidadMeses;
 
-            Console.Write("Lista clientes:");
-
-            foreach (var dir in sucursal.ObtenerClientes())
+            while(true)
             {
-                Console.WriteLine(dir["id"] + " - " + dir["nombre"] + " " + dir["apellido"]);
+                Console.Clear();
+                Console.WriteLine("Lista clientes:");
+
+                foreach (var dir in sucursal.ObtenerClientes())
+                {
+                    Console.WriteLine(dir["id"] + " - " + dir["nombre"] + " " + dir["apellido"]);
+                }
+
+                Console.WriteLine();
+                Console.Write("Ingrese la id del cliente (0 para salir): ");
+                int.TryParse(Console.ReadLine(), out id);
+                if (id == 0)
+                    break;
+
+                Console.Write("Ingrese el monto de la solicitud de prestamo: ");
+                int.TryParse(Console.ReadLine(), out monto);
+
+                Console.Write("Ingrese la cantidad de cuotas de la solicitud de prestamo: ");
+                int.TryParse(Console.ReadLine(), out cantidadMeses);
+
+                if (sucursal.SolicitarPrestamo(id, monto, cantidadMeses))
+                    Console.WriteLine("SOLICITUD VALIDA");
+                else
+                    Console.WriteLine("SOLICITUD NO VALIDA");
+
+                Console.ReadKey();
             }
 
-            Console.Write("Ingrese la id del cliente: ");
-            int.TryParse(Console.ReadLine(), out id);
-
-            Console.Write("Ingrese el monto de la solicitud de prestamo: ");
-            int.TryParse(Console.ReadLine(), out monto);
-
-            Console.Write("Ingrese la cantidad de meses de la solicitud de prestamo: ");
-            int.TryParse(Console.ReadLine(), out cantidadMeses);
-
-            Console.WriteLine(sucursal.SolicitarPrestamo(id, monto, cantidadMeses));
-
-            Console.ReadKey();
+            
 
 
         }
