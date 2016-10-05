ffe1f9353783ef79f6a0186ed5671aea62fac303
Black <Black@BLACK-NET>
2016-10-05 19:06:31 -0300

    Haciendo Fachada

---
+++ TP3/EJ4/Criptografia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ4
+{
+    class Criptografia
+    {
+
+        FabricaEncriptadores iFabEnc;
+
+        public Criptografia()
+        {
+            iFabEnc = FabricaEncriptadores.Instancia;
+        }
+
+        public string Encriptar(string pCadena, string pNombreEncriptador)
+        {
+            return iFabEnc.GetEncriptador(pNombreEncriptador).Encriptar(pCadena);
+        }
+
+        public string Desencriptar(string pCadena, string pNombreEncriptador)
+        {
+            return iFabEnc.GetEncriptador(pNombreEncriptador).Desencriptar(pCadena);
+        }
+
+    }
+}
