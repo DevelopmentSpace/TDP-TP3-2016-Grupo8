35d2644178c422632cab90c52dbfc181de838de7
Manuel Chichi <manuchichi@hotmail.com>
2016-10-05 18:34:33 -0300

    Fabrica de encriptadores terminada. xdxd

--- TP3/EJ4/FabricaEncriptadores.cs
+++ TP3/EJ4/FabricaEncriptadores.cs
@@ -8,5 +8,32 @@ namespace EJ4
 {
     class FabricaEncriptadores
     {
+        FabricaEncriptadores cInstancia;
+
+        Dictionary<string, IEncriptador> iEncriptadores;
+
+
+        private FabricaEncriptadores()
+        {
+            cInstancia = new FabricaEncriptadores();
+            iEncriptadores.Add("César", new EncriptadorCesar(12) );
+            iEncriptadores.Add("AES", new EncriptadorAES() );
+            iEncriptadores.Add("Null", new EncriptadorNulo());
+        }
+
+        public IEncriptador GetEncriptador(string nombre)
+        {
+            if (iEncriptadores.ContainsKey(nombre))
+                return iEncriptadores["Null"];
+            return iEncriptadores[nombre];
+        }
+
+        public FabricaEncriptadores Instancia
+            {
+            get {return this.cInstancia;}
+            }
+
+
+
     }
 }
